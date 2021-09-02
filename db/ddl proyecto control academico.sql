e6b110c67ac7a878d58b866cc423d21ff06b8adf
Denil José Parada Cabrera <dparada-2020225@kinal.edu.gt>
2021-09-02 09:46:36 -0600

    base de datos

---
+++ db/ddl proyecto control academico.sql
@@ -0,0 +1,107 @@
+DROP DATABASE IF EXISTS control_academico;
+CREATE DATABASE  IF NOT EXISTS control_academico;
+USE control_academico;
+
+-- -----------------------------------------------------
+-- Table alumno
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS alumno (
+  carne VARCHAR(16) NOT NULL,
+  apellidos VARCHAR(45) NULL,
+  nombres VARCHAR(45) NULL,
+  email VARCHAR(32) NULL,
+  PRIMARY KEY (carne))
+;
+
+
+-- -----------------------------------------------------
+-- Table Horario
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS Horario (
+  horario_id INT NOT NULL auto_increment,
+  horario_final TIME NOT NULL,
+  horario_inicio TIME NOT NULL,
+  PRIMARY KEY (horario_id))
+;
+
+
+-- -----------------------------------------------------
+-- Table instructor
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS instructor (
+  instructor_id INT NOT NULL auto_increment,
+  apellidos VARCHAR(45) NULL,
+  nombres VARCHAR(45) NULL,
+  direccion VARCHAR(45) NULL,
+  telefono VARCHAR(45) NULL,
+  PRIMARY KEY (instructor_id))
+;
+
+
+-- -----------------------------------------------------
+-- Table Carrera_tecnica
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS Carrera_tecnica (
+  codigo_carrera VARCHAR(128) NOT NULL,
+  nombres VARCHAR(45) NULL,
+  PRIMARY KEY (codigo_carrera))
+;
+
+
+-- -----------------------------------------------------
+-- Table Salon
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS Salon (
+  salon_id INT NOT NULL auto_increment,
+  capacidad INT NULL,
+  descripcion VARCHAR(255) NULL,
+  nombre_salon VARCHAR(255) NULL,
+  PRIMARY KEY (salon_id))
+;
+
+
+-- -----------------------------------------------------
+-- Table Curso
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS Curso (
+  curso_id INT NOT NULL auto_increment,
+  ciclo INT NULL,
+  cupo_maximo INT NULL,
+  cupo_minimo INT NULL,
+  descipcion VARCHAR(255) NULL,
+  codigo_carrera VARCHAR(128) NOT NULL,
+  horario_id INT NOT NULL,
+  instructor_id INT NOT NULL,
+  salon_id INT NOT NULL,
+  PRIMARY KEY (curso_id),
+  CONSTRAINT fk_Curso_Horario
+    FOREIGN KEY (horario_id)
+    REFERENCES Horario (horario_id),
+  CONSTRAINT fk_Curso_Salon
+    FOREIGN KEY (salon_id)
+    REFERENCES Salon (salon_id),
+  CONSTRAINT fk_Curso_Carrera_tecnica
+    FOREIGN KEY (codigo_carrera)
+    REFERENCES Carrera_tecnica (codigo_carrera),
+  CONSTRAINT fk_Curso_instructor
+    FOREIGN KEY (instructor_id)
+    REFERENCES instructor (instructor_id))
+;
+
+
+-- -----------------------------------------------------
+-- Table Asignacion_alumno
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS Asignacion_alumno (
+  asignacion_id INT NOT NULL auto_increment,
+  fecha_asignacion DATETIME NULL,
+  carne VARCHAR(16) NOT NULL,
+  curso_id INT NOT NULL,
+  PRIMARY KEY (asignacion_id),
+  CONSTRAINT fk_Asignacion_alumno_alumno
+    FOREIGN KEY (carne)
+    REFERENCES alumno (carne),
+  CONSTRAINT fk_Asignacion_alumno_Curso
+    FOREIGN KEY (curso_id)
+    REFERENCES Curso (curso_id))
+;
---
+++ db/dml proyecto control academico.sql
@@ -0,0 +1,90 @@
+-- -----------------------------------------------------
+-- Table alumno
+-- -----------------------------------------------------
+
+insert into alumno (carne, apellidos, nombres, email) 	
+values("2020-123","Contreras","Alvarado","AlvaradoC@gmail.com");
+insert into alumno (carne, apellidos, nombres, email) 	
+values("2020-234","Valdes","Juan","JuanV@gmail.com");
+insert into alumno (carne, apellidos, nombres, email) 	
+values("2020-345","Vasquez","Julio","JulioV@gmail.com");
+
+-- -----------------------------------------------------
+-- Table Horario
+-- -----------------------------------------------------
+
+insert into Horario (horario_final, horario_inicio) 	
+values("07:30:00","17:30:00");
+insert into Horario (horario_final, horario_inicio) 	
+values("17:30:00","07:30:00");
+insert into Horario (horario_final, horario_inicio) 	
+values("09:00:00","19:00:00");
+
+-- -----------------------------------------------------
+-- Table instructor
+-- -----------------------------------------------------
+
+insert into instructor (apellidos, nombres, direccion, telefono) 	
+values("Alvarez","Jose","direccion inventada 0-00","12345678");
+insert into instructor (apellidos, nombres, direccion, telefono) 	
+values("Monroy","Gabriela","direccion inventada 0-11","23456789");
+insert into instructor (apellidos, nombres, direccion, telefono) 	
+values("Hernadez","deysi","direccion inventada 0-22","34567891");
+
+-- -----------------------------------------------------
+-- Table Carrera_tecnica
+-- -----------------------------------------------------
+
+insert into Carrera_tecnica (codigo_carrera, nombres) 	
+values("CT01","Informatica");
+insert into Carrera_tecnica (codigo_carrera, nombres) 	
+values("CT02","Macanica");
+insert into Carrera_tecnica (codigo_carrera, nombres) 	
+values("CT03","Electronica");
+
+-- -----------------------------------------------------
+-- Table Salon
+-- -----------------------------------------------------
+
+insert into Salon (capacidad, descripcion, nombre_salon) 	
+values(200 ,"descripcion del salon inventada ","SL01");
+insert into Salon (capacidad, descripcion, nombre_salon) 	
+values(175 ,"descripcion del salon inventada ","SL02");
+insert into Salon (capacidad, descripcion, nombre_salon) 	
+values(180 ,"descripcion del salon inventada ","SL03");
+
+-- -----------------------------------------------------
+-- Table Curso
+-- -----------------------------------------------------
+
+insert into Curso (ciclo, cupo_maximo, cupo_minimo, descipcion, codigo_carrera, horario_id, instructor_id, salon_id) 	
+values(2020 ,50,25 ,"descripcion del curso inventada ","CT01",1,1,1);
+insert into Curso (ciclo, cupo_maximo, cupo_minimo, descipcion, codigo_carrera, horario_id, instructor_id, salon_id) 	
+values(2021 ,75,50 ,"descripcion del curso inventada ","CT02",2,2,2);
+insert into Curso (ciclo, cupo_maximo, cupo_minimo, descipcion, codigo_carrera, horario_id, instructor_id, salon_id) 	
+values(2022 ,100,75 ,"descripcion del curso inventada ","CT03",3,3,3);
+
+-- -----------------------------------------------------
+-- Table Asignacion_alumno
+-- -----------------------------------------------------
+
+insert into Asignacion_alumno (fecha_asignacion, carne, curso_id) 	
+values("2020-01-01 07:30:00","2020-123",1);
+insert into Asignacion_alumno (fecha_asignacion, carne, curso_id) 	
+values("2021-03-19 09:45:00","2020-234",2);
+insert into Asignacion_alumno (fecha_asignacion, carne, curso_id) 	
+values("2020-07-09 18:45:00","2020-345",3);
+
+-- -----------------------------------------------------
+-- Select Tablas
+-- -----------------------------------------------------
+
+select * from alumno;
+select * from Horario;
+select * from instructor;
+select * from Carrera_tecnica;
+select * from Salon;
+select * from Curso;
+select * from Asignacion_alumno;
+
+select salon_id, capacidad,descripcion,nombre_salon from Salon;
